4f46f7c8b2c1a2ab9e924586703bc7d01b667b07
elior <elior1001@gmail.com>
2023-03-22 21:50:28 +0200

    added db context all works for real now

---
+++ Server/Server/Data/MyDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
+namespace Server.Data
+{
+    // My data template
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+        }
+
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
