1c6e397df93b7a93716952adf1663697c8d3c64f
Victor Rutskin <Lashaka250375@gmail.com>
2023-03-23 11:52:29 +0200

    changin from \ to /
    because linux issues

--- Server/Server/Controllers/Car Controllers/CarController - Get.cs
+++ Server/Server/Controllers/Car Controllers/CarController - Get.cs
@@ -23,10 +23,10 @@ namespace Server.Controllers
                 foreach (var car in cars)
                 {
                     // If image exists use it, else use empty
-                    string imagePath = Paths.GetLocalPath() + @"\" + car.ImageSrc!;
+                    string imagePath = Paths.GetLocalPath() + @"/" + car.ImageSrc!;
                     if (System.IO.File.Exists(imagePath))
                     {
-                        car.ImageSrc = System.IO.File.ReadAllBytesAsync(Paths.GetLocalPath() + @"\" + car.ImageSrc!).ToString();
+                        car.ImageSrc = System.IO.File.ReadAllBytesAsync(Paths.GetLocalPath() + @"/" + car.ImageSrc!).ToString();
                     }
                     //no image src detected
                     else
@@ -69,10 +69,10 @@ namespace Server.Controllers
                 bool InvalidImageCheck = false;
 
                 // If image exists use it, else use empty
-                string imagePath = Paths.GetLocalPath() + @"\" + car.ImageSrc!;
+                string imagePath = Paths.GetLocalPath() + @"/" + car.ImageSrc!;
                 if (System.IO.File.Exists(imagePath))
                 {
-                    car.ImageSrc = System.IO.File.ReadAllBytesAsync(Paths.GetLocalPath() + @"\" + car.ImageSrc!).ToString();
+                    car.ImageSrc = System.IO.File.ReadAllBytesAsync(Paths.GetLocalPath() + @"/" + car.ImageSrc!).ToString();
                 }
                 //no image src detected
                 else
@@ -184,10 +184,10 @@ namespace Server.Controllers
                 {
 
                     // If image exists use it, else use empty
-                    string imagePath = Paths.GetLocalPath() + @"\" + car.ImageSrc!;
+                    string imagePath = Paths.GetLocalPath() + @"/" + car.ImageSrc!;
                     if (System.IO.File.Exists(imagePath))
                     {
-                        car.ImageSrc = System.IO.File.ReadAllBytesAsync(Paths.GetLocalPath() + @"\" + car.ImageSrc!).ToString();
+                        car.ImageSrc = System.IO.File.ReadAllBytesAsync(Paths.GetLocalPath() + @"/" + car.ImageSrc!).ToString();
                     }
                     else
                     {
